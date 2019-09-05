0bea3c74701100c6109c06143481c20510217928
devarcher <archershot@hotmail.com>
2019-09-05 17:15:24 -0500

    corrected breathing room for logo and hamburger menu on both desktop and mobile devices

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 "use strict";
 
-let body = document.getElementById("body");
+const body = document.getElementById("body");
+ 
 
 function switchLogo() {
   if (body.classList.contains('bg-white')) {
-    console.log("whiteBK");
+    // console.log("whiteBK");
     document.getElementById("logo").src = "./img/navbar/dark.svg";
   } else if (body.classList.contains('bg-dark')) {
     document.getElementById("logo").src = "./img/navbar/light.svg";
